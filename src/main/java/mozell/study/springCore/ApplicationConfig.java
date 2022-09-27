07a76bd9fc7587ad23db6f94d66012bc4a99fc87
mozell <mozellatto@gmail.com>
2022-09-27 22:54:59 +0900

    ApplicationContext 와 다양한 빈 설정방법 : 자바 componentScan

--- src/main/java/mozell/study/springCore/ApplicationConfig.java
+++ src/main/java/mozell/study/springCore/ApplicationConfig.java
@@ -1,22 +1,24 @@
 package mozell.study.springCore;
 
-import com.querydsl.core.annotations.Config;
 import mozell.study.springCore.book2.BookRepository;
 import mozell.study.springCore.book2.BookService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@Config
+@Configuration
+@ComponentScan(basePackageClasses = SpringCoreApplication.class)
 public class ApplicationConfig {
-
-    @Bean
-    public BookRepository bookRepository() {
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService() {
-        BookService bookService = new BookService();
-        bookService.setBookRepository(bookRepository());
-        return bookService;
-    }
+//
+//    @Bean
+//    public BookRepository bookRepository() {
+//        return new BookRepository();
+//    }
+//
+//    @Bean
+//    public BookService bookService() {
+//        BookService bookService = new BookService();
+//        bookService.setBookRepository(bookRepository());
+//        return bookService;
+//    }
 }
