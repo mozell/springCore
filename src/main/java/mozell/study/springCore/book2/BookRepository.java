a24279bad045f9121a029aca58c7ff17b77bd249
mozell <mozellatto@gmail.com>
2022-09-27 22:46:58 +0900

    ApplicationContext 와 다양한 빈 설정방법 : componentScan

--- src/main/java/mozell/study/springCore/book2/BookRepository.java
+++ src/main/java/mozell/study/springCore/book2/BookRepository.java
@@ -1,4 +1,7 @@
 package mozell.study.springCore.book2;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
--- src/main/java/mozell/study/springCore/book2/BookService.java
+++ src/main/java/mozell/study/springCore/book2/BookService.java
@@ -1,7 +1,12 @@
 package mozell.study.springCore.book2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
+    @Autowired
     BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
