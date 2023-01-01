1c6c6158e14ea9f1359efc71e14db42959849fe8
mozell <mozellatto@gmail.com>
2023-01-01 18:10:24 +0900

    @Autowired : 경우의 수, 해당 타입의 빈이 여러 개인 경우 - 해당 타입의 빈 모두 주입받기

--- src/main/java/mozell/study/springCore/book3/Book3Service.java
+++ src/main/java/mozell/study/springCore/book3/Book3Service.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class Book3Service {
 
     @Autowired
-    @Qualifier("mozellBook3Repository")
-    Book3Repository bookRepository;
+    List<Book3Repository> book3Repositories;
 
     public void printBook3Repository() {
-        System.out.println(bookRepository.getClass());
+        this.book3Repositories.forEach(System.out::println);
     }
 
 }
