8ba499f3212fb1b3b8ca199aa507ca92120955a0
mozell <mozellatto@gmail.com>
2023-01-03 22:20:53 +0900

    빈의 스코프 : 프로토타입

--- src/main/java/mozell/study/springCore/scope/Proto.java
+++ src/main/java/mozell/study/springCore/scope/Proto.java
@@ -1,7 +1,9 @@
 package mozell.study.springCore.scope;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Proto {
 }
