58e692fc817c6bc52967ed4ad78ddccfeb3eb7fd
mozell <mozellatto@gmail.com>
2023-01-10 22:29:50 +0900

    MessageSource 기본

---
+++ src/main/java/mozell/study/springCore/MessageSourceRunner.java
@@ -0,0 +1,22 @@
+package mozell.study.springCore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class MessageSourceRunner implements ApplicationRunner {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(messageSource.getMessage("greeting", new String[]{"mozell"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"mozell"}, Locale.getDefault()));
+    }
+}
