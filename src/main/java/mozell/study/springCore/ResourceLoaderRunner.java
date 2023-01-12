c18dd083f14a96a0cb6650ce988a432010462a80
mozell <mozellatto@gmail.com>
2023-01-12 23:37:10 +0900

    Validation 추상화, ValidationUtils 사용 (옛)

--- src/main/java/mozell/study/springCore/ResourceLoaderRunner.java
+++ src/main/java/mozell/study/springCore/ResourceLoaderRunner.java
@@ -1,5 +1,7 @@
 package mozell.study.springCore;
 
+import mozell.study.springCore.validation.Event;
+import mozell.study.springCore.validation.EventValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,21 +9,30 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @Component
 public class ResourceLoaderRunner implements ApplicationRunner {
 
-    @Autowired
-    ResourceLoader resourceLoader;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
+        Event event = new Event();
+        EventValidator eventValidator = new EventValidator();
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+
+        eventValidator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach( e -> {
+            System.out.println("======== error coed ========");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
