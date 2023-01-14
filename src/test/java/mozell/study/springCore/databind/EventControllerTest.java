0e913625cddc9fb443e666ceca7c5d8f629c4cb1
mozell <mozellatto@gmail.com>
2023-01-14 17:28:56 +0900

    데이터 바인딩 추상화:PropertyEditor, 옛날코딩

---
+++ src/test/java/mozell/study/springCore/databind/EventControllerTest.java
@@ -0,0 +1,28 @@
+package mozell.study.springCore.databind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getTest() throws Exception {
+        mockMvc.perform(get("/event/1"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("1"));
+    }
+}
