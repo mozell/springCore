57246893da02b87854a0f0dd76cc74d0d90cc7b5
mozell <mozellatto@gmail.com>
2023-01-14 18:11:42 +0900

    데이터 바인딩 추상화:Formatter

---
+++ src/main/java/mozell/study/springCore/databind/WebConfig.java
@@ -0,0 +1,14 @@
+package mozell.study.springCore.databind;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new EventFormatter());
+    }
+}
