7739f848153ba19dc3d5ae452d1ab8ed788d633a
mozell <mozellatto@gmail.com>
2023-01-15 14:50:49 +0900

    SpEL(스프링 Expression Language) 기본

---
+++ src/main/java/mozell/study/springCore/spel/SpELRunner.java
@@ -0,0 +1,40 @@
+package mozell.study.springCore.spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpELRunner implements ApplicationRunner {
+
+    @Value("#{1+1}")
+    int value;
+
+    @Value("#{'hello '+'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean tf;
+
+    @Value("hello")
+    String hello;
+
+    @Value("${my.value}")
+    int myValue;
+
+    @Value("#{${my.value} +100}")
+    int myValue2;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        System.out.println("===============");
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(tf);
+        System.out.println(hello);
+        System.out.println(myValue);
+        System.out.println(myValue2);
+    }
+}
